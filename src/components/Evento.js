6da967780362167cfe76f6f5305674a53ab9e641
flavia sigoli <flaviasivm@gmail.com>
2024-07-01 10:04:38 -0300

    aula 11

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,12 +1,19 @@
+import Button from "./evento/Button"
+
 function Evento() {
     function meuEvento() {
-        console.log("fui ativado")
+        console.log("Ativando primeiro evento!")
+    }
+
+    function segundoEvento() {
+        console.log('Ativando segundo evento!')
     }
 
     return (
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo Evento" />
         </div>
     )
 }
